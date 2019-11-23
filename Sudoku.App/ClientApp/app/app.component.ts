27669ecbf34f7c2844069491afd4be43fe60f083
Sergey Popov <dev.sergey.popov@gmail.com>
2019-11-23 21:57:53 +0300

    basic app

---
+++ Sudoku.App/ClientApp/app/app.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app',
+	styleUrls: ['app.css'],
+	templateUrl: 'app.html'
+})
+export class AppComponent {
+	currentView = 'game';
+
+	setView(view: string)
+	{
+		this.currentView = view;
+	};
+}
--- Sudoku.App/Startup.cs
+++ Sudoku.App/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.DependencyInjection;
 using Sudoku.App.Hubs;
 
@@ -20,7 +21,15 @@ namespace Sudoku.App
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    HotModuleReplacement = true
+                });
             }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseSignalR(routes =>
             {
